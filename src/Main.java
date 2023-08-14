fc72780d8b7b9bf9b2880f8ce77ed255f15e0c33
TatyanaSitnikova <soulofsmm@gmail.com>
2023-08-14 15:21:29 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int amound = 22000;
+        int bonus = 20;
+        int sum = amound / bonus;
+
+        System.out.println("Количество начисленных миль:" + sum);
+    }
+}
